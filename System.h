0b05ca5d8982a12ebd81bb135081b92201d27cff
Mario Alberto <mario_ds03@hotmail.com>
2014-05-07 01:05:04 -0500

    Create System.h

---
+++ System.h
@@ -0,0 +1,70 @@
+/*============================================================================*/
+/*                              TEAM 1				                          */
+/*============================================================================*/
+/*                           System Functions                                 */
+/*============================================================================*
+* C Include:        System.h
+* Instance:
+* version:          1
+* Created_by:       Mario Alberto Rivera González
+* Date_created:     Thu April 10 16:08:26 2014
+*=============================================================================*/
+/* DESCRIPTION : Header file for System Configuration                         */
+/*============================================================================*/
+/* FUNCTION COMMENT : The functions showed below allow users to configure     */
+/* System on FREEDOM KL25Z128									              */
+/*                                                                            */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*  REVISION |   DATE      |                               |      AUTHOR      */
+/*----------------------------------------------------------------------------*/
+/*  1.0      | 10/04/2014  | Configure all clock signals   |   Mario Rivera	  */
+/*============================================================================*/
+
+#ifndef SYSTEM_H_	/* To avoid double inclusion */
+#define SYSTEM_H_
+
+/* Includes */
+/* -------- */
+#include "stdtypedef.h"
+
+/* Exported types and constants */
+/* ---------------------------- */
+
+/* Types definition */
+
+/*==================================================*/
+/* Declaration of exported constants                */
+/*==================================================*/
+/* BYTE constants */
+
+/* WORD constants */
+
+/* LONG and STRUCTURE constants */
+
+
+/*======================================================*/
+/* Definition of RAM variables                          */
+/*======================================================*/
+/* BYTES */
+
+/* WORDS */
+
+/* LONGS and STRUCTURES */
+
+/*======================================================*/
+/* close variable declaration sections                  */
+/*======================================================*/
+
+/* Exported functions prototypes and macros */
+/* ---------------------------------------- */
+
+/* Functions prototypes */
+PUBLIC_FCT void SystemInit(void);
+
+/* Functions macros */
+
+/* Exported defines */
+
+#endif /* SYSTEM_H_ */
