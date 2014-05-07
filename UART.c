07ba4920c55125ef2b2f8fee3992fb3e34d96110
Carlos Isaac Avila Gutierrez <isaac.avila.gut@gmail.com>
2014-05-07 01:08:23 -0500

    Update UART.c
    Add start() and stop() functions. Init() Function was modified for auto calculating baud rate with minimum error coefficients

--- UART.c
+++ UART.c
@@ -21,6 +21,10 @@
 /*----------------------------------------------------------------------------*/
 /*  1.0      | 19/04/2014  | Add description for every     | Isaac Ávila      */
 /* 						   | function		               |      			  */
+/*----------------------------------------------------------------------------*/
+/*  2.0      | 24/04/2014  | Add functions start() and 	   | Isaac Ávila      */
+/* 			 |			   | stop(), modify init() for     |                  */
+/*			 |             | autocalculate BR registers    |                  */	
 /*============================================================================*/
 
 /* Includes */
@@ -71,12 +75,16 @@
  *  Return               :	None
  *  Critical/explanation :  No
  **************************************************************/
-void UART0_Init (void)
+void UART0_Init (T_ULONG bd_value)
 {
-	
-	T_ULONG br_value = 0;
-	T_UWORD osr_value = 24;				//Sampling ratio for less error percent (9,600 bps)
-	T_ULONG mcg_clk_hz = 21000000;	    //Frequency for FEI mode (Reset Mode)
+	T_ULONG baud_rate = 0;					//Variable to store baud rate
+	T_ULONG br_value = 0;					//Variable to store BD dividers
+	float br_float = 0;						//Auxiliar variable to save the result of division
+	float residue = 0;						//Variable to store the minimun residue
+	T_UBYTE temp = 4;						//Auxiliar Variable
+	T_UWORD osr_value = 4;					//Variable to store OSR dividers
+	const T_ULONG mcg_clk_hz = 21000000;	//FEI mode Frequency to UART0
+	T_UBYTE i = 0;							//Auxiliar Variable
 	
 	PORTA_PCR1 = PORT_PCR_MUX(2);       //Enable the UART_TXD function on PTA1 
 	PORTA_PCR2 = PORT_PCR_MUX(2);       //Enable the UART_TXD function on PTA2
@@ -85,20 +93,87 @@ void UART0_Init (void)
 	
 	SIM_SCGC4 |= SIM_SCGC4_UART0_MASK;  //Enable Clock signal for UART0 
   
-	UART0_C2 &= ~(UART0_C2_TE_MASK | UART0_C2_RE_MASK);     //Disable UART0 before changing registers
+	UART0_Stop();		//Disable UART0 before changing registers
 	
-	br_value = mcg_clk_hz / (baud_rate * (osr_value + 1));  //Calculate br value
+	baud_rate = bd_value;	//Baud rate desired	
+	
+	/*
+	 * Calculate BR registers depends on Baud Rate = Baud Clock / (BR * (OSR + 1)) according to datasheet
+	 */
+	br_value = mcg_clk_hz / (baud_rate * (osr_value + 1));
+	br_float = (float)mcg_clk_hz / ((float)baud_rate * ((float)osr_value + 1));
+	residue = br_float - (float)br_value;
+	
+	/*
+	 * Calculate it for all OSR possible values to find the minimum error
+	 */
+	for(i=5; i<=32;i++)
+	{
+		br_value = mcg_clk_hz / (baud_rate * (i + 1));
+		br_float = (float)mcg_clk_hz / ((float)baud_rate * ((float)i + 1));
+		br_float -= (float)br_value;
+		if(br_float < residue)		//If a new "minimum error" is found
+		{
+			residue = br_float;		//Store new minimum error
+			temp = i;				//Store OSR value for this minimum error
+		}
+	}
+	
+	osr_value = temp;				//Assign the OSR value founded to calculate the minimum error
+	br_value = mcg_clk_hz / (baud_rate * (osr_value + 1));	//Calculate BR value
 	
 	UART0_BDH |= UART0_BDH_SBR(((br_value & 0x1F00) >> 8)); //Setting BD dividers
 	UART0_BDL = (T_UBYTE)(br_value & UART0_BDL_SBR_MASK);   //Setting BD dividers
 
 	UART0_C4 = UART0_C4_OSR(osr_value);                     //Setting OSR for Sampling Ratio
 	
+	/*
+	 * If Sampling Ratio is less than 8
+	 */
+	if(osr_value < 8)
+	{
+		/*
+		 * Enable both edges of baud clock for receiving data
+		 */
+		UART0_C5|= UART0_C5_BOTHEDGE_MASK;	
+	}
+	else
+	{
+		
+	}
+	
 	UART0_C1 &=~(UART0_C1_M_MASK | UART0_C1_PE_MASK);       //8 Bit for data, No parity Selected
 	  
 	UART0_BDH &= ~(UART0_BDH_SBNS_MASK);                    //Configure One Stop Bit: 0 One stop bit.
 	
-	UART0_C2 |= (UART0_C2_TE_MASK | UART0_C2_RE_MASK );	    //Enable receiver and transmitter
+	/*
+	 * Enable receiver and transmitter
+	 */
+	UART0_Start();
+}
+
+/**************************************************************
+ *  Name                 :	UART0_Start
+ *  Description          :	Enables UART0's Receiver and Transmitter  
+ *  Parameters           :      None
+ *  Return               :	None
+ *  Critical/explanation :  No
+ **************************************************************/
+void UART0_Start()
+{
+	UART0_C2 |= (UART0_C2_TE_MASK | UART0_C2_RE_MASK );	//Enable receiver and transmitter	
+}
+
+/**************************************************************
+ *  Name                 :	UART0_Stop
+ *  Description          :	Enables UART0's Receiver and Transmitter  
+ *  Parameters           :      None
+ *  Return               :	None
+ *  Critical/explanation :  No
+ **************************************************************/
+void UART0_Stop()
+{
+	UART0_C2 &= ~(UART0_C2_TE_MASK | UART0_C2_RE_MASK);	//Disable transmitter and Receiver	
 }
 
 /**************************************************************
